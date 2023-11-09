d5659c6d14622150c10cf3da87d0457fa4801077
pabcrudel <pabcrudel@gmail.com>
2023-11-09 11:13:42 +0100

    add: Create Unit Testings for each function

---
+++ clear-array-in-palce/test.js
@@ -0,0 +1,37 @@
+import cleanArrayInPlace from './main.js';
+
+/** Unit Testings
+ * - [0] Raw Array
+ * - [1] Expected Clean Array
+ */
+const utArray = [
+  [
+    [
+      3, , [2,4,undefined, null], , 4, undefined, 5, null,
+      { a: 6 }, [ 7, 8, [null], null, 9 ], 10
+    ],
+    [
+      3, [2,4], 4, 5, { a: 6 }, [ 7, 8, [], 9 ], 10
+    ]
+  ],
+  [
+    [
+      , [undefined, null], , null,
+      [ [null], null, ],
+    ],
+    [
+      [],[[]]
+    ],
+  ],
+];
+
+const length = utArray.length;
+for (let i = 0; i < length; i++) {
+  console.log(`Test number: ${i + 1}`);
+  const test = utArray[i][0];
+  console.log("Original array: ", test);
+
+  cleanArrayInPlace(test);
+  console.log("Cleaned array: ", test);
+  console.log("Expected array: ", utArray[i][1], "\n");
+};
---
+++ insert-in-array/test.js
@@ -0,0 +1,49 @@
+import insertInArray from './main.js';
+
+/** Unit Testings
+ * - [0] Raw Array
+ * - [1] indexThatIsBefore
+ * - [2] arrayOrPrimitiveToInsert
+ * - [3] Expected Inserted Array
+ */
+const utArray = [
+  [
+    [ true, "hi", 4, "yes", null, 8 ],
+    3,
+    "bye",
+    [ true, "hi", 4, "yes", "bye", null, 8 ],
+  ],
+  [
+    [ true, "hi", 4, "yes", null, 8 ],
+    3,
+    ["bye", 3],
+    [ true, "hi", 4, "yes", "bye", 3, null, 8 ],
+  ],
+  [
+    [ true, "hi", 4, "yes", null, 8 ],
+    0,
+    null,
+    [ true, null, "hi", 4, "yes", null, 8 ],
+  ],
+];
+
+const length = utArray.length;
+for (let i = 0; i < length; i++) {
+  console.log(`\nTest number: ${i + 1}`);
+  
+  const test = utArray[i][0];
+  console.log("Original array: ", test);
+
+  const indexThatIsBefore = utArray[i][1];
+  console.log("indexThatIsBefore: ", indexThatIsBefore);
+
+  const arrayOrPrimitiveToInsert = utArray[i][2];
+  console.log("arrayOrPrimitiveToInsert: ", arrayOrPrimitiveToInsert);
+
+  const result =
+    insertInArray(test, indexThatIsBefore, arrayOrPrimitiveToInsert);
+  console.log("Inserted array: ", result);
+
+  console.log("Expected array: ", utArray[i][3]);
+};
+console.log();
