eb7702dd550b8d57c6ccb582d5c74767c41f7d9b
pabcrudel <pabcrudel@gmail.com>
2023-11-10 17:09:06 +0100

    change: Avoid markdown compilation error

--- utils/stringify-array/main.js
+++ utils/stringify-array/main.js
@@ -3,7 +3,9 @@
 function stringifyArray(arr) {
   const stringArr = JSON.stringify(arr, null, 0);
 
-  return stringArr === "[[{}]]" ? "[ [ {} ] ]" : stringArr;
+  var regExp = /\[\[\s*([^]*?)\s*\]\]/g;
+
+  return stringArr.replace(regExp, "[ [$1] ]");
 };
 
 module.exports = { stringifyArray };
