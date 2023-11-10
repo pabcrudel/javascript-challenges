4f8bcc9a4af5b83d22f145f1a65a73c39961d81d
pabcrudel <pabcrudel@gmail.com>
2023-11-10 22:07:01 +0100

    refactor: Use only allowed functions

--- utils/are-equal-arrays/main.js
+++ utils/are-equal-arrays/main.js
@@ -7,21 +7,22 @@ function areStrictEqualArraysNonRecursive(arr1, arr2) {
   // If they have different lengths, they aren't equals.
   if (len1 !== len2) return false;
 
-  // If both are numbers and NaN, they are equals.
-  const nanValidation = (item1, item2) =>
-  (typeof item1 === 'number' && isNaN(item1))
-  && (typeof item2 === 'number' && isNaN(item2));
-
   // If there are any item which is different, they aren't equals.
   for (let i = 0; i < len1; i++) {
     const item1 = arr1[i];
     const item2 = arr2[i];
 
     if (!nanValidation(item1, item2) && item1 !== item2 ) return false;
-  }
+  };
 
   // Else, they are equals
   return true;
+};
+
+/** If both are numbers and NaN, they are equals. */
+function nanValidation(item1, item2) {
+  return (typeof item1 === 'number' && isNaN(item1))
+  && (typeof item2 === 'number' && isNaN(item2));
 };
 
 module.exports = {
