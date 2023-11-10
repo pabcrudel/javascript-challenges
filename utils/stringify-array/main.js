83d070b9da46ba096c0f320df92c9aa765740be6
pabcrudel <pabcrudel@gmail.com>
2023-11-10 22:04:48 +0100

    change: Nullish value returned as strings

--- utils/stringify-array/main.js
+++ utils/stringify-array/main.js
@@ -1,11 +1,20 @@
 "use strict";
 
 function stringifyArray(arr) {
-  const stringArr = JSON.stringify(arr, null, 0);
+  const stringArr =
+    JSON.stringify(arr, (key, value) => stringifyNullish(key, value), 0);
 
   var regExp = /\[\[\s*([^]*?)\s*\]\]/g;
 
   return stringArr.replace(regExp, "[ [$1] ]");
+};
+
+function stringifyNullish(key, value) {
+  if (Number.isNaN(value)) return "NaN";
+  
+  if (typeof value === 'undefined') return "undefined";
+
+  return value;
 };
 
 module.exports = { stringifyArray };
