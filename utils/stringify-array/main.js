b77ca36262fdec1b8ed1ab565e77805a6dbca885
pabcrudel <pabcrudel@gmail.com>
2023-11-11 17:13:34 +0100

    add: Comment functions

--- utils/stringify-array/main.js
+++ utils/stringify-array/main.js
@@ -1,14 +1,23 @@
 "use strict";
 
+/** Stringify arrays in JSON format avoiding Markdown rendering errors and
+ * non nullish values representations. */
 function stringifyArray(arr) {
   const stringArr =
     JSON.stringify(arr, (key, value) => stringifyNullish(value), 0);
 
-  var regExp = /\[\[\s*([^]*?)\s*\]\]/g;
+  /**
+   * Markdown output table renders `[[<content>]]` as a broken link. In order
+   * to avoid that, I use this regular expression to catch it and display white
+   * space between square brackets: `[ [<content>] ]`.
+  */
+  var brokenLink = /\[\[\s*([^]*?)\s*\]\]/g;
 
-  return stringArr.replace(regExp, "[ [$1] ]");
+  return stringArr.replace(brokenLink, "[ [$1] ]");
 };
 
+/** JSON.stringify() changes all nullish values into null. However, in order to
+ * display these values, a string that represents them will be returned. */
 function stringifyNullish(value) {
   if (Number.isNaN(value)) return "NaN";
   
