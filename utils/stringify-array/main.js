8cf0e8843eff1cc77123a2e3cd5993f99dca5057
pabcrudel <pabcrudel@gmail.com>
2023-11-11 13:27:13 +0100

    change: Remove 'key' parameter

--- utils/stringify-array/main.js
+++ utils/stringify-array/main.js
@@ -2,14 +2,14 @@
 
 function stringifyArray(arr) {
   const stringArr =
-    JSON.stringify(arr, (key, value) => stringifyNullish(key, value), 0);
+    JSON.stringify(arr, (key, value) => stringifyNullish(value), 0);
 
   var regExp = /\[\[\s*([^]*?)\s*\]\]/g;
 
   return stringArr.replace(regExp, "[ [$1] ]");
 };
 
-function stringifyNullish(key, value) {
+function stringifyNullish(value) {
   if (Number.isNaN(value)) return "NaN";
   
   if (typeof value === 'undefined') return "undefined";
