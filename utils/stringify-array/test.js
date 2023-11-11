6a3dc28827c62879cfe1bd438f2f048414e6ae82
pabcrudel <pabcrudel@gmail.com>
2023-11-11 21:07:46 +0100

    add: Unit testing for stringify functions

---
+++ utils/stringify-array/test.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const { stringifyArray } = require('./main.js');
+const { utsFunction } = require('../unit-testings/main.js');
+
+/**
+ * - [0] Original entry
+ * - [1] Expected output
+ */
+const utsBattery = [
+  [
+    "[[{}]]",
+    "\"[ [{}] ]\""
+  ],
+  [
+    [[{}]],
+    "[ [{}] ]"
+  ],
+  [
+    "Hello [[world]]",
+    "\"Hello [ [world] ]\"",
+  ],
+  [
+    [2, "Hi", [[45n]]],
+    "[2,\"Hi\",[ [\"45n\"] ]]",
+  ],
+  [
+    [null, , [[undefined]]],
+    "[null,\"undefined\",[ [\"undefined\"] ]]",
+  ],
+  [
+    [2, [[[[,]]]], [[""]]],
+    "[2,[ [ [ [\"undefined\"] ] ] ], [ [\"\"] ]]",
+  ]
+];
+const utsTitle =
+  "Markdown link error and represent types not supported by JSON";
+const utsData = [
+  {
+    utTitle: "Stringify items",
+    utOutputHeadings: ["Expected output", "Output"],
+    utFunction: function(utItem){
+      const [ entry, expected ] = utItem;
+
+      const output = stringifyArray(entry);
+      const status = output === expected;
+  
+      const message = expected + " | " + output;
+  
+      return {status, message};
+    },
+  }
+];
+utsFunction(utsTitle, utsBattery, utsData);
+
